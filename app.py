b2df0fdba940683d2c1a066615a16a180133fb76
Leah Brady <brady.leah@gmail.com>
2021-01-11 19:38:38 -0500

    add results route, temp remove api route page

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ kidney_state_race_abo = Base.classes.kidney_state_race_abo_cleaned
 kidney_waitlist_additions = Base.classes.kidney_waitlist_additions_cleaned
 transplant_state_kidney = Base.classes.transplant_state_kidney_cleaned
 
-#Flask setup
+#Flask setup & save ML model as variable
 app = Flask(__name__)
+model = load_models("Bobs_model.h5")
 
 #TO DO: Flask Routes
 ##########################################################
@@ -41,27 +42,36 @@ app = Flask(__name__)
 def init():
     return render_template('index.html')
 
-@app.route("/apis")
-def api_routes():
-    return(
-        f"Available Routes:<br/>"
-        f"/api/v1.0/predict<br/>"
-        f"/api/v1.0/kidney_removal_state_ethnicity<br/>"
-        f"/api/v1.0/kidney_state_donor<br/>"
-        f"/api/v1.0/kidney_state_race_abo<br/>"
-        f"/api/v1.0/kidney_waitlist_additions<br/>"
-        f"/api/v1.0/transplant_state_kidney"
-    )
+# @app.route("/apis")
+# def api_routes():
+#     return(
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/predict<br/>"
+#         f"/api/v1.0/kidney_removal_state_ethnicity<br/>"
+#         f"/api/v1.0/kidney_state_donor<br/>"
+#         f"/api/v1.0/kidney_state_race_abo<br/>"
+#         f"/api/v1.0/kidney_waitlist_additions<br/>"
+#         f"/api/v1.0/transplant_state_kidney"
+#     )
 
 @app.route("/api/v1.0/predict")
 def predict():
-    model = load_models("Bobs_model.h5")
 
     user_input = [x for x in request.form.values()]
     features = [np.array(user_input)]
     prediction = model.predict(features)
 
     return jsonify(prediction)
+
+@app.route("/api/v1.0/results")
+def results():
+
+    data = request.get_json()
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True)
