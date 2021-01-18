df6e1ed4d94b201089a4c7f10eb3a88b1fdebaa7
Leah Brady <brady.leah@gmail.com>
2021-01-17 19:18:03 -0500

    begin potential scroll to top button

--- static/js/select_anchor.js
+++ static/js/select_anchor.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
         $('html,body').animate({ scrollTop: targetPosition}, 'slow');
     });
 });
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop()) {
+//         $('#toTop').fadeIn();
+//     } else {
+//         $('#toTop').fadeOut();
+//     }
+// });
+
+// $("#toTop").click(function () {
+//     $("html, body").animate({scrollTop: 0}, 1000);
+// });
